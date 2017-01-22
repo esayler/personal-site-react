5b231672c2536c899123ad65caa3b474606f10f4
Eric Sayler <easayler@gmail.com>
2017-01-22 12:50:39 -0700

    set up tooling, enable HMR, set up basic react app

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const { resolve } = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  context: __dirname,
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
+  devtool: 'eval-source-map',
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  output: {
+    path: resolve(__dirname, '/public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.scss', '.css']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?modules',
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+}
